36f17728379dcaf7c69305ab1c73df88ca614153
TimurBatyr <it.timurbatyr@gmail.com>
2022-06-02 18:50:59 +0600

    optimized main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
 import sys
-from os import path
 
 args = sys.argv
 print(args)
@@ -9,19 +7,17 @@ if len(args) != 2:
     print('Should be 1 argument')
     exit(0)
 
-commands = ['init', 'add', 'del', 'list']
+commands = ('init', 'add', 'del', 'list')
 
-if commands[0] == args[1]:
-    print(f'Была вызвана команда : {args[1]}')
+if args[1] in commands:
+    print(f'Command called is : {args[1]}')
+    exit(0)
+else:
+    print(f'Such command does not exist')
+    exit(0)
 
-if commands[1] == args[1]:
-    print(f'Была вызвана команда : {args[1]}')
 
-if commands[2] == args[1]:
-    print(f'Была вызвана команда : {args[1]}')
 
-if commands[3] == args[1]:
-    print(f'Была вызвана команда : {args[1]}')
 
 
 
