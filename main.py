8106e06f40984512884bdac32ee944572254dfb9
TimurBatyr <it.timurbatyr@gmail.com>
2022-06-03 17:28:33 +0600

    Done with step7

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
+import os
 import sys
+from pathlib import Path
 
 args = sys.argv
 print(args)
 
-if len(args) != 2:
-    print('Should be 1 argument')
+if len(args) != 3:
+    print('Should not be more than 3 argument')
+
+if len(args) == 1:
+    print('Add arguments')
     exit(0)
 
-commands = ('init', 'add', 'del', 'list')
+commands = {
+    'init': 'init_fs.py',
+    'add': 'add_file.py',
+    'del': 'del_file.py',
+    'list': 'list_files.py',
+    'get': 'get_file',
+}
+
 
 if args[1] in commands:
     print(f'Command called is : {args[1]}')
-    exit(0)
-else:
+
+elif not args[1] in commands:
     print(f'Such command does not exist')
     exit(0)
 
+
+#Step-7
+if args[1] == list(commands)[0]:
+    os.system(f'python3 {commands.get(args[1])}')
+
+elif args[1] == list(commands)[1]:
+    os.system(f'python3 {commands.get(args[1])} {args[2]}')
+
+elif args[1] == list(commands)[2]:
+    os.system(f'python3 {commands.get(args[1])} {args[2]}')
+
+elif args[1] == list(commands)[3]:
+    os.system(f'python3 {commands.get(args[1])}')
 
 
 
