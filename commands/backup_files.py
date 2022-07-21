68733de25eb5de4f9ae5f41a6dbe37aefd6c6940
TimurBatyr <it.timurbatyr@gmail.com>
2022-07-21 17:46:48 +0600

    refactored scripts on backup n restore

--- commands/backup_files.py
+++ commands/backup_files.py
@@ -1,3 +1,7 @@
+import fcntl
+import time
+
+import flock
 import os
 from os import path
 from zipfile import ZipFile
@@ -6,13 +10,13 @@ BASE_DIR = '.zeon_git'
 
 
 def confirm_backup():
-    confirm = input("Are you sure you want to run backup? (y/n): ").lower()
+    confirm = input('Are you sure you want to run backup? (y/n): ').lower()
 
-    if confirm == "y":
+    if confirm == 'y':
         if not path.exists(BASE_DIR):
             print('Does not exist such dir')
 
-    elif confirm == "n":
+    elif confirm == 'n':
         exit()
     else:
         print("Use only 'y' or 'n'")
@@ -24,9 +28,13 @@ def backupfiles():
 
     zipfile = ZipFile('backup.zip', 'w')
     for root, dirs, files in os.walk(BASE_DIR):
-        for file in files:
-            zipfile.write(os.path.join(root, file))
+        for f in files:
+            with open(os.path.join(root, f), 'a') as file:
+                fcntl.flock(file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                zipfile.write(os.path.join(root, f))
+                # os.access(os.path.join(root, file))
+                # time.sleep(10)
+                fcntl.flock(file, fcntl.LOCK_UN)
     zipfile.close()
-
 
 
--- commands/restore_files.py
+++ commands/restore_files.py
@@ -4,42 +4,54 @@ from os import path
 from zipfile import ZipFile
 
 BASE_DIR = '.zeon_git'
+INTERIM_DIR = '.zeon_git_interim'
 
 
 def confirm_restore():
-    confirm = input("Are you sure you want to run restore? (y/n): ").lower()
+    confirm = input('Are you sure you want to run restore? (y/n): ').lower()
 
-    if confirm == "y":
+    if confirm == 'y':
         if not path.exists('backup.zip'):
             print('Does not exist such zipfile')
             exit()
 
-    elif confirm == "n":
+    elif confirm == 'n':
         exit()
     else:
         print("Use only 'y' or 'n'")
         exit()
 
 
-def del_git():
-    for item in os.listdir(BASE_DIR):
-        file_path = os.path.join(BASE_DIR, item)
+def del_zeon_git(dir):
+    for item in os.listdir(dir):
+        file_path = os.path.join(dir, item)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
-            print('The content of .zeon_git was deleted')
+            print(f'The content of {dir} was deleted')
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
 
 
 def restorefiles():
     confirm_restore()
-    del_git()
+
+    if not path.isdir(BASE_DIR):
+        print('Such dir does not exist')
+        exit()
 
     file_name = "backup.zip"
     with ZipFile(file_name, 'r') as zip:
         zip.printdir()
-        zip.extractall('.')
+        zip.extractall(INTERIM_DIR)
         print('Restoration done!')
+
+    if path.isdir(BASE_DIR):
+        del_zeon_git(BASE_DIR)
+
+    os.system(f'mv {INTERIM_DIR}/.zeon_git ./')
+
+    if path.isdir(BASE_DIR):
+        os.rmdir(INTERIM_DIR)
